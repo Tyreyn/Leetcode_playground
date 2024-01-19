2cc38134379676e55978240efd35c3aff34601ca
Tyreyn <athosikish33@gmail.com>
2024-01-19 19:12:15 +0100

    49. Group anagrams, 1.Two Sum

---
+++ LeetCodeTests/HashMapTestData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTests
+{
+    public static class HashMapTestData
+    {
+        public static IEnumerable<object[]> TD_GroupAnagrams()
+        {
+            yield return new object[]
+            {
+                new string[] { "eat", "tea", "tan", "ate", "nat", "bat" },
+                new List<List<string>>
+                {
+                    new List<string> {"bat"},
+                    new List<string> {"nat","tan"},
+                    new List<string> {"ate","eat","tea"}
+                },
+            };
+            yield return new object[]
+            {
+                new string[] { "" },
+                new List<List<string>>
+                {
+                    new List<string> {{""}}
+                },
+            };
+            yield return new object[]
+            {
+                new string[] { "a" },
+                new List<List<string>>
+                {
+                    new List<string> { { "a" } }
+                },
+            };
+            yield return new object[]
+            {
+                null,
+                null,
+            };
+        }
+    }
+}
--- leetcode_playground/HashMaps.cs
+++ leetcode_playground/HashMaps.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace leetcode_playground
 {
@@ -89,6 +91,74 @@ namespace leetcode_playground
                 }
             }
             return true;
+        }
+
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+            Dictionary<char, int> letterDictionary = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                if (!letterDictionary.ContainsKey(c))
+                {
+                    letterDictionary.Add(c, 1);
+                }
+                else if (letterDictionary.ContainsKey(c))
+                {
+                    letterDictionary[c]++;
+                }
+            }
+
+            foreach (char c in t)
+            {
+                if (letterDictionary.ContainsKey(c) && letterDictionary[c] > 0)
+                {
+                    letterDictionary[c]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                int tmp = target - nums[i];
+                if (result.ContainsKey(tmp)) return new int[] { result[tmp], i };
+                result[nums[i]] = i;
+
+            }
+
+            return new int[] { };
+        }
+
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dict = new Dictionary<string, IList<string>>();
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                char[] arr = strs[i].ToCharArray();
+                Array.Sort(arr);
+                String sorted = new String(arr);
+
+                if (!dict.ContainsKey(sorted))
+                {
+                    dict.Add(sorted, new List<String>() { strs[i] });
+                }
+                else
+                {
+                    dict[sorted].Add(strs[i]);
+                }
+            }
+
+            return dict.Values.ToList();
         }
     }
 }
